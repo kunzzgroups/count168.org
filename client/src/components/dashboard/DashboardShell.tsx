9058fcef47382f00bc70f0ed1a6e17d75e5a5cf6
kunzzit01 <kunzzit01@gmail.com>
2026-04-23 17:02:23 +0800

    68

--- client/src/components/dashboard/DashboardShell.tsx
+++ client/src/components/dashboard/DashboardShell.tsx
@@ -104,25 +104,6 @@ export function DashboardShell({
 
   return (
     <div className={shellClass}>
-      {classicSidebarLayout && (
-        <div
-          id="user-avatar"
-          role="button"
-          tabIndex={0}
-          aria-label="打开菜单"
-          onClick={() => setNavOpen(true)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-              e.preventDefault()
-              setNavOpen(true)
-            }
-          }}
-        >
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden>
-            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-          </svg>
-        </div>
-      )}
       {classicSidebarLayout ? (
         <header className="dShell__classicMobileHeader">
           <button
