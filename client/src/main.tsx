398372856c1859b88158381ed6cdad87ed4f048e
kunzzit01 <kunzzit01@gmail.com>
2026-04-23 14:42:44 +0800

    29

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
@@ -9,11 +8,9 @@ import App from './App.tsx'
 const basename = getRouterBasename()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter basename={basename}>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
-  </StrictMode>,
+  <ErrorBoundary>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>,
 )
