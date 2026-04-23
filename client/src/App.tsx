92bfbb09b00f85be95c557bb25db56a12ff88237
kunzzit01 <kunzzit01@gmail.com>
2026-04-23 17:55:32 +0800

    80

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { DashboardPage } from './pages/DashboardPage'
+import { DataCapturePage } from './pages/DataCapturePage'
 import { TransactionPage } from './pages/TransactionPage'
 import { LoginPage } from './pages/LoginPage'
 import { OwnerSecondaryPasswordPage } from './pages/OwnerSecondaryPasswordPage'
@@ -13,6 +14,7 @@ export default function App() {
         element={<OwnerSecondaryPasswordPage />}
       />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/datacapture" element={<DataCapturePage />} />
       <Route path="/transaction" element={<TransactionPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
