ef6abf17784aaff4a88fce6442152058fafe1662
kunzzit01 <kunzzit01@gmail.com>
2026-04-23 10:15:34 +0800

    c168

--- frontend/src/constants/authStorage.ts
+++
@@ -1,2 +0,0 @@
-export const AUTH_TOKEN_KEY = 'count168_auth_token'
-export const AUTH_USER_KEY = 'count168_auth_user'
--- frontend/src/lib/apiError.ts
+++
@@ -1,9 +0,0 @@
-export class ApiRequestError extends Error {
-  readonly errorCode: string | undefined
-
-  constructor(message: string, errorCode?: string) {
-    super(message)
-    this.name = 'ApiRequestError'
-    this.errorCode = errorCode
-  }
-}
