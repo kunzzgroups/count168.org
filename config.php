0d262e9986e0363f577f2d0237ef3b35b9e488b4
kunzzit01 <kunzzit01@gmail.com>
2026-05-05 13:54:47 +0800

    100

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
-$dbname = 'u857194726_count168_org';
-$dbuser = 'u857194726_count168_org';
+$dbname = 'u857194726_c168_org';
+$dbuser = 'u857194726_c168_org';
 $dbpass = 'Kunzz_c168_org';
 
 // 设置PHP时区为马来西亚时间
