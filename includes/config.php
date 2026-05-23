1301abb3bb9c241bc6e97efb122240061d33d63e
Jack <hwang6373>
2026-05-23 22:23:43 +0800

    config c168.com

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'u857194726_c168site';
-$dbuser = 'u857194726_c168site';
-$dbpass = 'C168_site';
+$dbname = 'u857194726_count168';
+$dbuser = 'u857194726_count168';
+$dbpass = 'Kholdings1688@';
 
 // 设置PHP时区为马来西亚时间
 date_default_timezone_set('Asia/Kuala_Lumpur');
